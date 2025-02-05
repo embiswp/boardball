653fc1288bfa12a06d0876eb5f0d6aa9979c9624
Simon Péter <simonpetymeg@gmail.com>
2025-02-05 13:45:04 +0000

    Test for Rows are even

--- BoardBall.Core.Tests/GameTests.cs
+++ BoardBall.Core.Tests/GameTests.cs
@@ -6,17 +6,19 @@ namespace BoardBall.Core.Tests;
 public class GameTests
 {
     [Fact]
-    public void Test() {
-        var test=1;
+    public void Test()
+    {
+        var test = 1;
         test.ShouldBe(1);
     }
 
     [Fact]
-    public void Test_Game_Start_Sets_Properties() {
+    public void Test_Game_Start_Sets_Properties()
+    {
         //Given
         var game = new Game();
         //When
-        game.Start("Péter","Balázs",7,9,10);
+        game.Start("Péter", "Balázs", 7, 9, 10);
         //Then
         game.Player1.ShouldBe("Péter");
         game.Player2.ShouldBe("Balázs");
@@ -26,25 +28,29 @@ public class GameTests
     }
 
     [Fact]
-    public void Test_After_Successful_Game_Start_Game_State_Is_Playing() {
+    public void Test_After_Successful_Game_Start_Game_State_Is_Playing()
+    {
         //Given
         var game = new Game();
         //When
-        game.Start("Péter","Balázs",7,9,10);
+        game.Start("Péter", "Balázs", 7, 9, 10);
         //Then
         game.State.ShouldBe(GameState.Playing);
     }
 
     [Fact]
-    public void Test_Player1_Name_Is_Empty() {
+    public void Test_Player1_Name_Is_Empty()
+    {
         //Given
         var game = new Game();
         //When
-        try {
-            game.Start("","Balázs",7,9,10);
+        try
+        {
+            game.Start("", "Balázs", 7, 9, 10);
             Assert.Fail();
         }
-        catch (ArgumentException exception) {
+        catch (ArgumentException exception)
+        {
             System.Console.WriteLine(exception);
             //Then
             exception.Message.ShouldBe("player1");
@@ -52,27 +58,47 @@ public class GameTests
     }
 
     [Fact]
-    public void Test_Player2_Name_Is_Empty(){
+    public void Test_Player2_Name_Is_Empty()
+    {
         //Given
         var game = new Game();
         //When
-        try {
-            game.Start("Péter", "", 0,0,0);
+        try
+        {
+            game.Start("Péter", "", 0, 0, 0);
             Assert.Fail();
         }
-        catch(ArgumentException exception){
+        catch (ArgumentException exception)
+        {
             System.Console.WriteLine(exception);
             //Then
             exception.Message.ShouldBe("player2");
         }
     }
 
-    //Test for error case when Columns are even
     //Test for error case when Rows are even
+    [Fact]
+    public void Test_Rows_are_even()
+    {
+        var game = new Game();
+        try
+        {
+            game.Start("Péter", "Balázs", 2, 0, 0);
+            Assert.Fail();
+        }
+        catch (ArgumentException exception)
+        {
+            Console.WriteLine("SKILL ISSEU?!");
+            Console.WriteLine(exception);
+            exception.Message.ShouldBe("rows");
+        }
+
+    }
+    //Test for error case when Columns are even
     //Test for error case when Columns are 0 or less than 3
     //Test for error case when Rows are 0 or less than 1
     //Test for error case when number of footballers is odd
-    
+
 }
 
 public enum GameState
@@ -93,10 +119,15 @@ public class Game
     public void Start(string player1, string player2, int rows, int columns, int footballers)
     {
         if (string.IsNullOrEmpty(player1)) throw new ArgumentException(nameof(player1));
-        if (string.IsNullOrEmpty(player2)) throw new ArgumentException(nameof(player2));
         Player1 = player1;
+        if (string.IsNullOrEmpty(player2)) throw new ArgumentException(nameof(player2));
         Player2 = player2;
-        Rows = rows;
+        if (columns % 2 != 0) {
+            Rows = rows;
+        }
+        else{
+            throw new ArgumentException(nameof(rows));
+        }
         Columns = columns;
         Footballers = footballers;
         State = GameState.Playing;
