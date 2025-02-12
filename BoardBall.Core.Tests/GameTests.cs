ba5bbdc813204e8910750957aeef07c9c8edf662
Simon Péter <simonpetymeg@gmail.com>
2025-02-12 19:06:03 +0000

    just some comment

--- BoardBall.Core.Tests/GameTests.cs
+++ BoardBall.Core.Tests/GameTests.cs
@@ -137,9 +137,10 @@ public class GameTests
             exception.Message.ShouldBe("rows");
         }
     }
-    //Test for error case when Rows are 0 or less than 1
+    
     //Test for error case when number of footballers is odd
 
+    // next step is the splitting
 }
 
 public enum GameState
